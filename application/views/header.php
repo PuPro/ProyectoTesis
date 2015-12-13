72261c9f60eedbd864bb4482ddb1129c555429ef
solangeCampos <solange.camposcampos@gmail.com>
2015-12-13 02:20:20 -0300

    validacion modulo trabajador
    validacion campos vacio

--- application/views/header.php
+++ application/views/header.php
@@ -2,12 +2,18 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link href="<?= base_url() ?>../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="<?= base_url() ?>../css/style.css" rel="stylesheet">
-        <link href="<?= base_url() ?>../css/container.css" rel="stylesheet">
         <script src="<?= base_url() ?>../Jquery/jquery-1.11.3.min.js"></script>
         <script src="<?= base_url() ?>../Bootstrap/js/bootstrap.min.js"></script>
         <script src="<?= base_url() ?>../js/funcion.js"></script>
+        <link href="<?= base_url() ?>../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>../css/style.css" rel="stylesheet">
+        <link href="<?= base_url() ?>../css/container.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>../css/jquery-ui.css">
+        <script type="text/javascript" src="<?=base_url()?>../js/jquery.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>../js/jquery-ui.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="<?= base_url() ?>../css/container.css">
+
         <script type="text/javascript">
             //Captura lo que la direccion que contiene base_url
             var base_url = '<?= base_url(); ?>';
@@ -19,7 +25,7 @@
             <div id="encabezado">
                 <div id="login"></div>
             </div>
-                <div id="content">
+            <div id="content">
 
-                        
-               
+
+
